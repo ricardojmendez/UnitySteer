401a1d6203d6af2526edce70c5d1dd0d0de7aa05
Ricardo J. Mendez <ricardo@arges-systems.com>
2010-06-26 14:09:21 +0300

    Removed ad-hocery from Vehicle.cs
    On his comments, CWR himself mentioned he wasn't sure that the default
    should be the AdjustRawSteeringForce he referred to as an "ad-hocery".
    Removing, since I'm aiming to simply this as much as possible.

--- UnitySteer/Behaviors/Vehicle.cs
+++ UnitySteer/Behaviors/Vehicle.cs
@@ -6,11 +6,7 @@ using System.Collections;
 public class Vehicle: MonoBehaviour
 {
 	#region Internal state values
-	float _curvature;
-	Vector3 _lastForward;
-	Vector3 _lastPosition;
 	Vector3 _smoothedAcceleration;
-	float _smoothedCurvature;
 	Vector3 _smoothedPosition;
 	#endregion
 
@@ -165,7 +161,7 @@ public class Vehicle: MonoBehaviour
 		_steerings = this.GetComponents<Steering>();
 	}
 
-	void FixedUpdate()
+	void Update()
 	{
 		foreach (var steering in _steerings)
 		{
@@ -185,12 +181,10 @@ public class Vehicle: MonoBehaviour
 	/// </param>
 	private void ApplySteeringForce(Vector3 force, float elapsedTime)
 	{
-		if (MaxForce == 0 || MaxSpeed == 0)
+		if (MaxForce == 0 || MaxSpeed == 0 || elapsedTime == 0)
 		{
 			return;
 		}
-
-		force = AdjustRawSteeringForce(force);
 
 		// enforce limit on magnitude of steering force
 		Vector3 clippedForce = OpenSteerUtility.truncateLength(force, MaxForce);
@@ -205,30 +199,16 @@ public class Vehicle: MonoBehaviour
 
 		Vector3 newVelocity = Velocity;
 
-		// damp out abrupt changes and oscillations in steering acceleration
-		// (rate is proportional to time step, then clipped into useful range)
-		#if CLIPPING_ELAPSEDTIME
 		/*
+			Damp out abrupt changes and oscillations in steering acceleration
+			(rate is proportional to time step, then clipped into useful range)
+			
 			The lower the smoothRate parameter, the more noise there is
 			likely to be in the movement.
 		 */
-		if (elapsedTime > 0)
-		{
-			/*
-				RJM: The clipping of smoothRate is framerate-dependent, which
-				is bad when we're trying to get consistent behavior across a
-				variety of processor usages.
-			 */
-			float smoothRate = Mathf.Clamp(9 * elapsedTime, 0.15f, 0.4f);
-			_smoothedAcceleration = OpenSteerUtility.blendIntoAccumulator(smoothRate,
-										newAcceleration,
-										_smoothedAcceleration);
-		}
-		#else
 		_smoothedAcceleration = OpenSteerUtility.blendIntoAccumulator(0.4f,
 									newAcceleration,
 									_smoothedAcceleration);
-		#endif
 
 		// Euler integrate (per frame) acceleration into velocity
 		newVelocity += _smoothedAcceleration * elapsedTime;
@@ -241,22 +221,36 @@ public class Vehicle: MonoBehaviour
 
 		// Euler integrate (per frame) velocity into position
 		// TODO: Change for a motor
-		transform.position += (newVelocity * elapsedTime);
+		if (rigidbody == null)
+		{
+			transform.position += (newVelocity * elapsedTime);
+		}
+		else
+		{
+			/*
+			 * TODO: This is just a quick test and should not remain, as the behavior is not
+			 * consistent to that we obtain when moving the transform.
+			 */
+			rigidbody.AddForce(Speed * elapsedTime * transform.forward, ForceMode.Impulse);
+		}
+		
 
 		// regenerate local space (by default: align vehicle's forward axis with
 		// new velocity, but this behavior may be overridden by derived classes.)
 		RegenerateLocalSpace (newVelocity);
-
-		// maintain path curvature information
-		MeasurePathCurvature (elapsedTime);
-
+		
 		// running average of recent positions
 		_smoothedPosition = OpenSteerUtility.blendIntoAccumulator(elapsedTime * 0.06f,
 								transform.position,
 								_smoothedPosition);
 	}
-
-
+	
+	protected virtual void RegenerateLocalSpace (Vector3 newVelocity)
+	{
+ 		if (Speed > 0)
+			transform.forward = newVelocity / Speed;
+	}	
+	
 	/// <summary>
 	/// Adjust the steering force passed to ApplySteeringForce.
 	/// </summary>
@@ -268,76 +262,11 @@ public class Vehicle: MonoBehaviour
 	/// </returns>
 	/// <remarks>
 	/// Allows a specific vehicle class to redefine this adjustment.
-	/// default is to disallow backward-facing steering at low speed.
-	///
-	/// xxx should the default be this ad-hocery, or no adjustment?
-	/// xxx experimental 8-20-02
-	///
-	/// NOTE RJM: Upon profiling, this seems to be about 25% of what
-	/// applySteeringForce is doing. It might be worth reviewing if it
-	/// should be kept around, considering that CWR was unsure if this
-	/// "ad-hocery" should remain as default.
-	///
-	/// This sort of adjustment is definitely useful for vehicles such
-	/// as the lightning chain, but might not need to be on the base
-	/// class.
+	/// The default will return the value unmodified.
 	/// </remarks>
-	public virtual Vector3 AdjustRawSteeringForce(Vector3 force)
+	protected virtual Vector3 AdjustRawSteeringForce(Vector3 force)
 	{
-		// Do force adjustment only if the speed is a fifth of our
-		// maximum valid speed
-		float maxAdjustedSpeed = 0.2f * MaxSpeed;
-
-		if ((Speed > maxAdjustedSpeed) || (force == Vector3.zero))
-		{
-			return force;
-		}
-		else
-		{
-			float range = Speed / maxAdjustedSpeed;
-			float cosine = Mathf.Lerp(1.0f, -1.0f, Mathf.Pow(range, 20));
-			Vector3 angle = OpenSteerUtility.limitMaxDeviationAngle(force, cosine, transform.forward);
-			return angle;
-		}
-	}
-
-	void RegenerateLocalSpace (Vector3 newVelocity)
-	{
-		// TODO: We should apply maximum turning speed here.
-		// adjust orthonormal basis vectors to be aligned with new velocity
-		if (Speed > 0)
-			transform.forward = newVelocity / Speed;
-	}
-
-
-	void MeasurePathCurvature (float elapsedTime)
-	{
-		if (elapsedTime > 0)
-		{
-			Vector3 pos = transform.position;
-			Vector3 fwd = transform.forward;
-			Vector3 dP = _lastPosition - pos;
-			Vector3 dF = (_lastForward - fwd) / dP.magnitude;
-			//SI - BIT OF A WEIRD FIX HERE . NOT SURE IF ITS CORRECT
-			//Vector3 lateral = dF.perpendicularComponent (forward ());
-			Vector3 lateral = OpenSteerUtility.perpendicularComponent(dF, fwd);
-
-			float sign = (Vector3.Dot(lateral, transform.right) < 0) ? 1.0f : -1.0f;
-			_curvature = lateral.magnitude * sign;
-			/*
-				If elapsedTime is greater than 0.25, that means that blendIntoAccumulator
-				will end up clipping the first parameter to [0..1], and we'll lose information,
-				making this call framerate-dependent.
-
-				No idea where that 4.0f value comes from, probably out of a hat.
-			 */
-			_smoothedCurvature = OpenSteerUtility.blendIntoAccumulator(elapsedTime * 4.0f,
-									_curvature,
-									_smoothedCurvature);
-
-			_lastForward = fwd;
-			_lastPosition = pos;
-		}
+		return force;
 	}
 	#endregion
 }
