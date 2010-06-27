4c544586ebb82ae2238b9ff999a052920415fa62
Ricardo J. Mendez <ricardo@arges-systems.com>
2010-06-27 23:55:33 +0300

    Removed outdated behaviours
    ComboBehavior was no longer relevant after Vehicle became an actual
    combination of behaviors.  Reformatted code while I was at it.

--- UnitySteer/Behaviors/Vehicle.cs
+++ UnitySteer/Behaviors/Vehicle.cs
@@ -322,58 +322,58 @@ public class Vehicle: MonoBehaviour
     }	
 	
 	
-		/// <summary>
-		/// Calculates if a vehicle is in the neighborhood of another
-		/// </summary>
-		/// <param name="other">
-		/// Another vehicle to check against<see cref="Vehicle"/>
-		/// </param>
-		/// <param name="minDistance">
-		/// Minimum distance <see cref="System.Single"/>
-		/// </param>
-		/// <param name="maxDistance">
-		/// Maximum distance <see cref="System.Single"/>
-		/// </param>
-		/// <param name="cosMaxAngle">
-		/// Cosine of the maximum angle between vehicles (for performance)<see cref="System.Single"/>
-		/// </param>
-		/// <returns>
-		/// True if within the neighborhood, or false if otherwise<see cref="System.Boolean"/>
-		/// </returns>
-		/// <remarks>Originally SteerLibrary.inBoidNeighborhood</remarks>
-		public bool IsInNeighborhood (Vehicle other, float minDistance, float maxDistance, float cosMaxAngle)
+	/// <summary>
+	/// Calculates if a vehicle is in the neighborhood of another
+	/// </summary>
+	/// <param name="other">
+	/// Another vehicle to check against<see cref="Vehicle"/>
+	/// </param>
+	/// <param name="minDistance">
+	/// Minimum distance <see cref="System.Single"/>
+	/// </param>
+	/// <param name="maxDistance">
+	/// Maximum distance <see cref="System.Single"/>
+	/// </param>
+	/// <param name="cosMaxAngle">
+	/// Cosine of the maximum angle between vehicles (for performance)<see cref="System.Single"/>
+	/// </param>
+	/// <returns>
+	/// True if within the neighborhood, or false if otherwise<see cref="System.Boolean"/>
+	/// </returns>
+	/// <remarks>Originally SteerLibrary.inBoidNeighborhood</remarks>
+	public bool IsInNeighborhood (Vehicle other, float minDistance, float maxDistance, float cosMaxAngle)
+	{
+		if (other == this)
 		{
-			if (other == this)
+			return false;
+		}
+		else
+		{
+			Vector3 offset = other.transform.position - transform.position;
+			float distanceSquared = offset.sqrMagnitude;
+
+			// definitely in neighborhood if inside minDistance sphere
+			if (distanceSquared < (minDistance * minDistance))
 			{
-				return false;
+				return true;
 			}
 			else
 			{
-				Vector3 offset = other.transform.position - transform.position;
-				float distanceSquared = offset.sqrMagnitude;
-
-				// definitely in neighborhood if inside minDistance sphere
-				if (distanceSquared < (minDistance * minDistance))
+				// definitely not in neighborhood if outside maxDistance sphere
+				if (distanceSquared > (maxDistance * maxDistance))
 				{
-					return true;
+					return false;
 				}
 				else
 				{
-					// definitely not in neighborhood if outside maxDistance sphere
-					if (distanceSquared > (maxDistance * maxDistance))
-					{
-						return false;
-					}
-					else
-					{
-						// otherwise, test angular offset from forward axis
-						Vector3 unitOffset = offset / (float) System.Math.Sqrt (distanceSquared);
-						float forwardness = Vector3.Dot(transform.forward, unitOffset);
-						return forwardness > cosMaxAngle;
-					}
+					// otherwise, test angular offset from forward axis
+					Vector3 unitOffset = offset / (float) System.Math.Sqrt (distanceSquared);
+					float forwardness = Vector3.Dot(transform.forward, unitOffset);
+					return forwardness > cosMaxAngle;
 				}
 			}
 		}
+	}
 		
 	
 	/// <summary>
