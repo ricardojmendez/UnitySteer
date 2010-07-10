d8d73e4654bc8c04a0474fa1f4ced91b068d4a15
Ricardo J. Mendez <ricardo@arges-systems.com>
2010-07-10 17:20:22 +0300

    Clarified that CharacterMotorVehicle is not a key component

--- Behaviors/CharacterMotorVehicle.cs
+++ Behaviors/CharacterMotorVehicle.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 
 /// <summary>
-/// Vehicle subclass that acts as an interface to CharacterMotor.  You'll need
-/// to have imported the Character Controllers from Standard Assets for it to
-/// build.
+/// Vehicle subclass that acts as an interface to CharacterMotor.
 /// </summary>
+/// <remarks>
+/// IMPORTANT:You'll need to have imported the Character Controllers 
+/// from Standard Assets for it to build, or you can just exclude it
+/// from your project as it is not a key component.
+/// </remarks>
 [RequireComponent(typeof(CharacterMotor))]
 public class CharacterMotorVehicle: Vehicle
 {
