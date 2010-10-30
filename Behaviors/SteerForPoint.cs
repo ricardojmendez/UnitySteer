a5a9834f612c243bf39ee8048608672c97064085
Ricardo J. Mendez <ricardo@arges-systems.com>
2010-10-30 10:27:55 +0300

    Added OnArrival event to SteerForPoint

--- Behaviors/SteerForPoint.cs
+++ Behaviors/SteerForPoint.cs
@@ -1,12 +1,24 @@
 using UnityEngine;
+using UnitySteer.Helpers;
 
 public class SteerForPoint: Steering
 {
+	bool _reportedArrival = false;
+	SteeringEventHandler<Vector3> _onArrival;
+	
 	/// <summary>
 	/// The target point.
 	/// </summary>
  	public Vector3 TargetPoint;
 	
+	public SteeringEventHandler<Vector3> OnArrival {
+		get {
+			return this._onArrival;
+		}
+		set {
+			_onArrival = value;
+		}
+	}	
 	
 	void Awake() {
 		if (TargetPoint == Vector3.zero)
@@ -21,7 +33,17 @@ public class SteerForPoint: Steering
 	/// </returns>
 	protected override Vector3 CalculateForce()
 	{
-		return Vehicle.GetSeekVector(TargetPoint);
+		var force = Vehicle.GetSeekVector(TargetPoint);
+		
+		// Raise the arrival event
+		if (!_reportedArrival && _onArrival != null && force == Vector3.zero) {
+			_reportedArrival = true;
+			_onArrival(new SteeringEvent<Vector3>(this, "arrived", TargetPoint));
+		}
+		else
+			_reportedArrival = force == Vector3.zero;
+		
+		return force;
 	}
 }
 
