44a0dad8abdeeeb2125cb7cd1a637fb09302b14b
Ricardo J. Mendez <ricardo@arges-systems.com>
2011-01-29 14:08:31 +0200

    Added SteerForSpeed behavior

---
+++ Behaviors/SteerForSpeed.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+/// <summary>
+/// Behavior that will aim to achieve a constant speed along the vehicle's forward vector
+/// </summary>
+public class SteerForSpeed : Steering {
+	
+	/// <summary>
+	/// Speed that the behavior will aim to achieve
+	/// </summary>
+	[SerializeField]
+	private float _targetSpeed = 5;
+	
+	
+	public float TargetSpeed 
+	{
+		get 
+		{
+			return this._targetSpeed;
+		}
+		set 
+		{
+			_targetSpeed = value;
+		}
+	}	
+
+	/// <summary>
+	/// Calculates the force to apply to a vehicle to reach a target transform
+	/// </summary>
+	/// <returns>
+	/// Force to apply <see cref="Vector3"/>
+	/// </returns>
+	protected override Vector3 CalculateForce()
+	{
+		return Vehicle.GetTargetSpeedVector(TargetSpeed);
+	}
+}
