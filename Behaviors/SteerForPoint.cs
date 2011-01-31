22d8aec45fe17611fefccfaf44cb4907955f3f1a
Ricardo J. Mendez <ricardo@arges-systems.com>
2011-01-31 12:23:08 +0200

    SteerForPoint now using the base OnArrival notification handler

--- Behaviors/SteerForPoint.cs
+++ Behaviors/SteerForPoint.cs
@@ -3,26 +3,11 @@ using UnitySteer.Helpers;
 
 public class SteerForPoint: Steering
 {
-	bool _reportedArrival = false;
-	SteeringEventHandler<Vector3> _onArrival;
-	
 	/// <summary>
 	/// The target point.
 	/// </summary>
  	public Vector3 TargetPoint;
-	
-	public SteeringEventHandler<Vector3> OnArrival 
-	{
-		get 
-		{
-			return this._onArrival;
-		}
-		set 
-		{
-			_onArrival = value;
-		}
-	}	
-	
+		
 	void Awake() {
 		if (TargetPoint == Vector3.zero)
 			TargetPoint = transform.position;
@@ -36,17 +21,7 @@ public class SteerForPoint: Steering
 	/// </returns>
 	protected override Vector3 CalculateForce()
 	{
-		var force = Vehicle.GetSeekVector(TargetPoint, false);
-		
-		// Raise the arrival event
-		if (!_reportedArrival && _onArrival != null && force == Vector3.zero) {
-			_reportedArrival = true;
-			_onArrival(new SteeringEvent<Vector3>(this, "arrived", TargetPoint));
-		}
-		else
-			_reportedArrival = force == Vector3.zero;
-		
-		return force;
+		return Vehicle.GetSeekVector(TargetPoint, false);
 	}
 }
 
