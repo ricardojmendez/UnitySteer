07b543e856df3c8f7f79d781a1f9604ed5a8dac7
Ricardo J. Mendez <ricardo@arges-systems.com>
2013-09-17 14:47:14 +0300

    New SteerForPathSimplified properties for path distance, percentage

--- Behaviors/SteerForPathSimplified.cs
+++ Behaviors/SteerForPathSimplified.cs
@@ -10,15 +10,13 @@ using UnitySteer;
 /// </remarks>
 [AddComponentMenu("UnitySteer/Steer/... for PathSimplified")]
 public class SteerForPathSimplified : Steering
-{
-	
+{	
 	#region Private fields
 	[SerializeField]
 	float _predictionTime = 1.5f;
 	
 	[SerializeField]
 	float _minSpeedToConsider = 0.25f;
-	
 	#endregion
 	
 	
@@ -33,6 +31,22 @@ public class SteerForPathSimplified : Steering
 		set {
 			_predictionTime = value;
 		}
+	}
+
+	/// <summary>
+	/// How far along the path we were the last time we calculated forces?
+	/// </summary>
+	/// <value>The last path distance evaluated.</value>
+	public float DistanceAlongPath { get; private set; }
+
+
+	/// <summary>
+	/// What percentage of the path we had traversed when we last evaluated?
+	/// </summary>
+	/// <value>The last path percentage evaluated.</value>
+	public float PathPercentTraversed 
+	{ 
+		get { return (Path != null) ? DistanceAlongPath / Path.TotalPathLength : 0; }
 	}
 	
 	/// <summary>
@@ -78,7 +92,7 @@ public class SteerForPathSimplified : Steering
 		float pathDistanceOffset = _predictionTime * speed;
 		
 		// measure distance along path of our current and predicted positions
-		float currentPathDistance = Path.MapPointToPathDistance (Vehicle.Position);
+		DistanceAlongPath = Path.MapPointToPathDistance (Vehicle.Position);
 		
 		/*
 		 * Otherwise we need to steer towards a target point obtained
@@ -97,7 +111,7 @@ public class SteerForPathSimplified : Steering
 		 * bends and turns and is not a straight vector projected away
 		 * from our position.
 		 */
-		float targetPathDistance = currentPathDistance + pathDistanceOffset;
+		float targetPathDistance = DistanceAlongPath + pathDistanceOffset;
 		var target = Path.MapPathDistanceToPoint (targetPathDistance);
 		
 		/*
