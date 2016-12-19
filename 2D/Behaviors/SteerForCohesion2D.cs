7014a91a71db397fefbafe5a75b053d02b7dd5a9
Steve Streeting <steve@stevestreeting.com>
2016-12-19 11:38:03 +0000

    Add min/max cohesion contribution for more controllable flocking

--- 2D/Behaviors/SteerForCohesion2D.cs
+++ 2D/Behaviors/SteerForCohesion2D.cs
@@ -11,14 +11,17 @@ namespace UnitySteer2D.Behaviors
     [RequireComponent(typeof (SteerForNeighborGroup2D))]
     public class SteerForCohesion2D : SteerForNeighbors2D
     {
+        [SerializeField] private float _minContribution = 0f;
+        [SerializeField] private float _maxContribution = 1f;
+
         public override Vector2 CalculateNeighborContribution(Vehicle2D other)
         {
             // accumulate sum of forces leading us towards neighbor's positions
             var distance = other.Position - Vehicle.Position;
             var sqrMag = distance.sqrMagnitude;
-            // Provide some contribution, but diminished by the distance to 
+            // Provide some contribution, but diminished by the distance to
             // the vehicle.
-            distance *= 1 / sqrMag;
+            distance *= Mathf.Clamp(1 / sqrMag, _minContribution, _maxContribution);
             return distance;
         }
 
