07662fba8f25098c633769e030c404cf0c439015
makadimilan <makadi.milan.mate@gmail.com>
2017-05-11 04:34:11 +0200

    batchmode and nogui

--- .deploy/travis/run_unit_tests.sh
+++ .deploy/travis/run_unit_tests.sh
@@ -12,13 +12,20 @@ then
 fi
 
 echo "\nRunning unit tests.\n"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -runEditorTests -projectPath ./ -editorTestsResultFile ./testresults.xml
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+ -batchmode \
+ -nographics \
+ -runEditorTests \
+ -projectPath ./ \
+ -editorTestsResultFile ./testresults.xml
+ 
 exitcode=$?;
 
-echo "\nTest result saved to ./testresults.xml\n"
+echo "\nTest result saved to ./testresults.xml\n\t - results:"
 cat ./testresults.xml
 
 if [[ $exitcode != 0 ]]; 
-then 
+then
+	echo "\nUnit tests are FAILED, exit code is $exitcode\n"
 	exit $exitcode; 
 fi
