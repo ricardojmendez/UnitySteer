344235b0f4d6defc1b26698889feae2455933093
makadimilan <makadi.milan.mate@gmail.com>
2017-05-11 04:50:07 +0200

    fallback

--- .deploy/travis/run_unit_tests.sh
+++ .deploy/travis/run_unit_tests.sh
@@ -18,8 +18,36 @@ echo "\nRunning unit tests.\n"
  -runEditorTests \
  -projectPath ./ \
  -editorTestsResultFile ./testresults.xml
- 
+
 exitcode=$?;
+
+# fallback
+if [[ $exitcode != 0 ]]; 
+then
+	echo "\nRunning unit tests without batchmode.\n"
+	/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+	 -nographics \
+	 -runEditorTests \
+	 -projectPath ./ \
+	 -editorTestsResultFile ./testresults.xml
+	 exitcode=$?;
+fi
+
+# fallback
+if [[ $exitcode != 0 ]]; 
+then
+	echo "\nRunning unit tests without nographics.\n"
+	/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+	 -runEditorTests \
+	 -projectPath ./ \
+	 -editorTestsResultFile ./testresults.xml
+	 exitcode=$?;
+fi
+
+echo "DEBUG - exitcode is $exitcode"
+echo "DEBUG - ls -al ./"
+ls -al ./
+
 
 echo "\nTest result saved to ./testresults.xml\n\t - results:"
 cat ./testresults.xml
